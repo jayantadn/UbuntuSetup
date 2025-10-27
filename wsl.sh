f3b6cfac35b59f2138e43b7929c6ac92eb847bd2
Jayanta <jyd1kor@bosch.com>
2025-10-27 18:35:15 +0530

    Add gedit installation to wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -8,3 +8,6 @@ sudo apt install thunar-archive-plugin
 sudo apt install file-roller
 sudo apt install zip unzip p7zip-full rar unrar
 sudo apt install -y xfce4-terminal xterm
+
+# install other GUI tools
+sudo apt install gedit
