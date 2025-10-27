72c55e64505e99abaf50df88b9c71991aab4043c
Jayanta <jyd1kor@bosch.com>
2025-10-27 18:20:45 +0530

    Replace thunar plugins installation with terminal packages

--- wsl.sh
+++ wsl.sh
@@ -7,4 +7,4 @@ sudo apt install thunar
 sudo apt install thunar-archive-plugin
 sudo apt install file-roller
 sudo apt install zip unzip p7zip-full rar unrar
-sudo apt install thunar-volman thunar-media-tags-plugin
+sudo apt install -y xfce4-terminal xterm
