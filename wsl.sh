4759042aa39965dff4d788dc7a7e10161f98923c
Jayanta <jyd1kor@bosch.com>
2025-10-29 17:35:47 +0530

    Consolidate package installations in wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -3,11 +3,7 @@
 echo "Running WSL-specific setup..."
 
 # install thunar filemanager
-sudo apt install thunar
-sudo apt install thunar-archive-plugin
-sudo apt install file-roller
-sudo apt install zip unzip p7zip-full rar unrar
-sudo apt install -y xfce4-terminal xterm
+sudo apt install -y thunar thunar-archive-plugin file-roller zip unzip p7zip-full rar unrar xfce4-terminal xterm
 
 # install other GUI tools
-sudo apt install gedit
+sudo apt install -y gedit
